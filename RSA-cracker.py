bf4a0f249f697f9625e241062f6d821bfdf8f7c4
mehfluffy <alissa.cheng@protonmail.ch>
2021-01-04 11:58:32 +0100

    Implement crack_rsa

--- RSA-cracker.py
+++ RSA-cracker.py
@@ -1,23 +1,9 @@
 from functools import reduce
-from numpy import lcm, gcd, floor
+from math import gcd, floor
 
 def factorize(n):    
-    factors = list(
-        reduce(
-            list.__add__,
-            ([i, n // i] for i in range(1, int(n**0.5) + 1) if n % i == 0)
-        )
-    )
-    return factors
-
-def gcd_euclid_ext(a, b):  # doesn't work to find the inverse
-    '''a*x + b*y = gcd(a, b)'''
-    if a == 0:
-        return 0, 1, b
-    x1, y1, gcd = gcd_euclid_ext(b%a, a)
-    x = y1 - (b // a) * x1
-    y = x1
-    return x, y, gcd
+    factors = [[i, n//i] for i in range(2, int(n**0.5)+1) if n%i==0]   # square root = biggest factor
+    return set(reduce(list.__add__, factors)) # flatten and make into set (faster, no repeated element)
 
 
 def use_rsa():
@@ -26,17 +12,17 @@ def use_rsa():
     q = int(input("Enter prime 2, different from prime 1: "))
     n = p * q
 
-    # compute phi, lambda:
+    # compute phi and lambda:
     phi = (p - 1) * (q - 1)
-    lam = lcm(p - 1, q - 1)
+    #lam = lcm(p - 1, q - 1)
 
     # choose e:
     e_choices = []
-    for i in range(1, lam):
-        if phi % i != 0 and len(factorize(i)) == 2:
+    for i in range(1, phi):
+        if gcd(i, phi) == 1:
             e_choices.append(i)
     if len(e_choices) > 1:
-        e = int(input(f"Choose e as one of {e_choices}:"))
+        e = int(input(f"Choose e as one of {e_choices}: "))
     elif len(e_choices) == 1:
         e = e_choices[0]
         print("Only 1 choice for e found, using:", e)
@@ -45,7 +31,6 @@ def use_rsa():
         return
 
     # compute d:
-    #d = e ** (phi - 1) % phi   # this works, but isn't correct
     k = 1
     while k > 0:
         d = (1 + k*phi) / e
@@ -55,16 +40,6 @@ def use_rsa():
             break
         else:
             k += 1
-    '''  # this doesn't work
-    d1, d2, g = gcd_euclid_ext(e, n)
-    d = list(filter(lambda x: x>0, [d1, d2]))
-    if g == 1 and len(d) == 1:
-        d = d[0]
-        print("Private key found,", d)
-    else:
-        print("Something went wrong, exiting.")
-        return
-    '''
 
     # encrypt and decrypt:
     enc_keys = [n, e]
@@ -77,33 +52,45 @@ def use_rsa():
     print("Encrypted message:", message_enc)
     print("Decrypted message:", message_dec)
 
+    return enc_keys, message_enc
 
-def crack_rsa():
-    # find out p and q:
-    pq = factorize(enc_keys[0])  # factorize n
+
+def crack_rsa(enc_keys, message_enc):
+    n, e = enc_keys
+    # by factorizing n:
+    pq = factorize(n)
+    print("Factors of n found:", pq)
 
     # find k such that (k * (p - 1) * (q - 1) + 1) is divisible by e, the quotient would then be d.
     while pq:
         p_test = min(pq)
         q_test = max(pq)
+        phi_test = (p_test-1) * (q_test-1)
 
         k_test = 1
-        while k < 65537 + 1:  # in practice is commonly 65537
-            cond_test = k_test * (p_test - 1) * (q_test - 1) + 1
-            if cond_test % e == 0:
-                break
+        while k_test < 6:  # debug
+            test = 1 + k_test * phi_test
+            if test % e == 0:
+                break  # inner loop
             else: k_test += 1
-
-        d_guess = cond_test / e
+        
+        d_guess = int(test / e)  # integer since test % e == 0
+        message_dec = message_enc ** d_guess % n
+        print("Private key found:", d_guess)
+        print("Decrypted message:", message_dec)
+        break
+        '''
         if d_guess != d:
             pq -= {p_test, q_test}
         elif d_guess == d:
-            print("Private decoding key found:", d_guess)
+            print("Private key found:", d_guess)
             break
+        '''
 
 
 def main():
-    use_rsa()
+    enc_keys, message_enc = use_rsa()
+    crack_rsa(enc_keys, message_enc)
 
 if __name__ == '__main__':
     main()
